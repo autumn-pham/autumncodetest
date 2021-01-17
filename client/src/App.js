c526a346772c123a12b7f4b1cbc8cf19f89a29f1
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2021-01-17 15:09:35 -0800

    moved state into app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import Header from './components/Header';
 import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    books: [],
+    currentBook: {},
+    currentIndex: 0
+  }
+
+  componentDidMount = () => {
+    axios
+    .get('http://localhost:4000/books')
+    .then(response => {
+      this.setState({
+        books: response.data,
+        currentBook: response.data[0]
+      })
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  }
+
+  deleteBook = (event) => {
+    axios.delete('http://localhost:4000/books/' + event.target.value).then(response => {
+      this.setState({
+        books: response.data
+      })
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  }
+
+  updateBook = (event) => {
+    event.preventDefault();
+    const id = event.target.id
+    axios.put('http://localhost:4000/books/' + id, this.state).then(response => {
+      this.setState({
+        books: response.data
+      })
+    })
+  }
+
+  nextBook = () => {
+   this.setState({
+     currentIndex: this.currentIndex + 1,
+     currentBook: this.state.books[this.state.currentIndex]
+   })
+ }
 
   render() {
     return (
@@ -13,7 +61,12 @@ class App extends React.Component {
         <hr />
         <Form />
         <hr />
-        <Card />
+        <Card
+          books={this.state.books}
+          currentBook={this.state.currentBook}
+          onNextBook={this.nextBook}
+          onDelete={this.deleteBook}
+        />
       </div>
     )
   }
--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,110 +1,19 @@
 import React from 'react';
-import axios from 'axios';
-
-class Card extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      // books: []
-      books: [],
-      currentBook: {},
-      currentIndex: 0
-    }
-
-    this.nextBook = this.nextBook.bind(this)
-  }
 
 
-  componentDidMount = () => {
-    axios
-    .get('http://localhost:4000/books')
-    .then(response => {
-      this.setState({
-        books: response.data,
-        currentBook: response.data[0]
-      })
-    })
-    .catch(function(error){
-      console.log(error);
-    })
-  }
-
-  // componentDidMount = () => {
-  //   axios
-  //   .get('http://localhost:4000/books')
-  //   .then(response => {
-  //     this.setState({
-  //       books: response.data
-  //     })
-  //   })
-  //   .catch(function(error){
-  //     console.log(error);
-  //   })
-  // }
-
-
-
-  deleteBook = (event) => {
-    axios.delete('http://localhost:4000/books/' + event.target.value).then(response => {
-      this.setState({
-        books: response.data
-      })
-    })
-    .catch(function(error){
-      console.log(error);
-    })
-  }
-
-  updateBook = (event) => {
-    event.preventDefault();
-    const id = event.target.id
-    axios.put('http://localhost:4000/books/' + id, this.state).then(response => {
-      this.setState({
-        books: response.data
-      })
-    })
-  }
-
-  nextBook = () => {
-   this.setState({
-     currentIndex: this.currentIndex + 1,
-     currentBook: this.state.books[this.state.currentIndex]
-   })
- }
-
-  render() {
-    return (
-      <div className="card text-center" key={this.state.currentBook._id}>
-          <img src={this.state.currentBook.img} className="card-img-top" alt="book cover" />
-          <div className="card-body">
-            <h4>{this.state.currentBook.title}</h4>
-            <h5>{this.state.currentBook.author}</h5>
-            <p className="card-text">{this.state.currentBook.factoid}</p>
-            <button className="btn btn-danger" value={this.state.currentBook._id} onClick={this.deleteBook} type="submit" name="action">
-              DELETE
-            </button>
-            <button className="btn btn-primary" onClick={this.nextBook}>Next</button>
-          </div>
+const Card = ({ books, currentBook, onNextBook, onDelete }) => {
+  return (
+    <div className="card text-center" key={currentBook._id}>
+        <img src={currentBook.img} className="card-img-top" alt="book cover" />
+        <div className="card-body">
+          <h4>{currentBook.title}</h4>
+          <h5>{currentBook.author}</h5>
+          <p className="card-text">{currentBook.factoid}</p>
+          <button className="btn btn-danger" value={currentBook._id} onClick={onDelete} type="submit" name="action">DELETE</button>
+          <button className="btn btn-primary" onClick={onNextBook}>Next</button>
         </div>
-
-    // <div className="card-columns">
-    // { this.state.books.map(book => {
-    //   return <div className="card text-center" key={book._id}>
-    //     <img src={book.img} className="card-img-top" alt="book cover" />
-    //     <div className="card-body">
-    //       <h4>{book.title}</h4>
-    //       <h5>{book.author}</h5>
-    //       <p className="card-text">{book.factoid}</p>
-    //       <button className="btn btn-danger delete-btn" value={book._id} onClick={this.deleteBook} type="submit" name="action">
-    //         DELETE
-    //         </button>
-    //     </div>
-    //     </div>
-    // })}
-    // </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Card;
