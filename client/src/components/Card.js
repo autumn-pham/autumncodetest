f3cb4a18e25b55ca6ad79d8b753496643a640e2c
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2021-01-17 14:44:00 -0800

    working on state

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -6,41 +6,42 @@ class Card extends React.Component {
     super(props);
 
     this.state = {
-      books: []
-      // books: [],
-      // currentBook: 0
+      // books: []
+      books: [],
+      currentBook: {},
+      currentIndex: 0
     }
 
-    // this.nextBook = this.nextBook.bind(this)
+    this.nextBook = this.nextBook.bind(this)
   }
 
-
-  // componentDidMount = () => {
-  //   axios
-  //   .get('http://localhost:4000/books')
-  //   .then(response => {
-  //     this.setState({
-  //       books: response.data,
-  //       currentBook: response.data[0]
-  //     })
-  //   })
-  //   .catch(function(error){
-  //     console.log(error);
-  //   })
-  // }
 
   componentDidMount = () => {
     axios
     .get('http://localhost:4000/books')
     .then(response => {
       this.setState({
-        books: response.data
+        books: response.data,
+        currentBook: response.data[0]
       })
     })
     .catch(function(error){
       console.log(error);
     })
   }
+
+  // componentDidMount = () => {
+  //   axios
+  //   .get('http://localhost:4000/books')
+  //   .then(response => {
+  //     this.setState({
+  //       books: response.data
+  //     })
+  //   })
+  //   .catch(function(error){
+  //     console.log(error);
+  //   })
+  // }
 
 
 
@@ -50,62 +51,58 @@ class Card extends React.Component {
         books: response.data
       })
     })
+    .catch(function(error){
+      console.log(error);
+    })
   }
 
-  updateBook = event => {
+  updateBook = (event) => {
     event.preventDefault();
     const id = event.target.id
     axios.put('http://localhost:4000/books/' + id, this.state).then(response => {
       this.setState({
-        books: response.data,
-        title: '',
-        author: '',
-        img: '',
-        factoid: ''
+        books: response.data
       })
     })
   }
 
- //  nextBook() {
- //   if (this.currentBook < this.state.books.length - 1) {
- //     return this.setState({
- //       currentBook: this.currentBook + 1
- //     })
- //   }
- // }
-
-
+  nextBook = () => {
+   this.setState({
+     currentIndex: this.currentIndex + 1,
+     currentBook: this.state.books[this.state.currentIndex]
+   })
+ }
 
   render() {
     return (
-    // <div className="card text-center" key={this.state.currentBook._id}>
-    //       <img src={this.state.currentBook.img} className="card-img-top" alt="book cover" />
-    //       <div className="card-body">
-    //         <h4>{this.state.currentBook.title}</h4>
-    //         <h5>{this.state.currentBook.author}</h5>
-    //         <p className="card-text">{this.state.currentBook.factoid}</p>
-    //         <button className="btn btn-danger" value={this.state.currentBook._id} onClick={this.deleteBook} type="submit" name="action">
-    //           DELETE
-    //         </button>
-    //         <button onClick={this.nextBook}>Next</button>
-    //       </div>
-    //   </div>
-
-    <div className="card-columns">
-    { this.state.books.map((book) => {
-      return <div className="card text-center" key={book._id}>
-        <img src={book.img} className="card-img-top" alt="book cover" />
-        <div className="card-body">
-          <h4>{book.title}</h4>
-          <h5>{book.author}</h5>
-          <p className="card-text">{book.factoid}</p>
-          <button className="btn btn-danger delete-btn" value={book._id} onClick={this.deleteBook} type="submit" name="action">
-            DELETE
+      <div className="card text-center" key={this.state.currentBook._id}>
+          <img src={this.state.currentBook.img} className="card-img-top" alt="book cover" />
+          <div className="card-body">
+            <h4>{this.state.currentBook.title}</h4>
+            <h5>{this.state.currentBook.author}</h5>
+            <p className="card-text">{this.state.currentBook.factoid}</p>
+            <button className="btn btn-danger" value={this.state.currentBook._id} onClick={this.deleteBook} type="submit" name="action">
+              DELETE
             </button>
+            <button className="btn btn-primary" onClick={this.nextBook}>Next</button>
+          </div>
         </div>
-        </div>
-    })}
-    </div>
+
+    // <div className="card-columns">
+    // { this.state.books.map(book => {
+    //   return <div className="card text-center" key={book._id}>
+    //     <img src={book.img} className="card-img-top" alt="book cover" />
+    //     <div className="card-body">
+    //       <h4>{book.title}</h4>
+    //       <h5>{book.author}</h5>
+    //       <p className="card-text">{book.factoid}</p>
+    //       <button className="btn btn-danger delete-btn" value={book._id} onClick={this.deleteBook} type="submit" name="action">
+    //         DELETE
+    //         </button>
+    //     </div>
+    //     </div>
+    // })}
+    // </div>
     )
   }
 }
